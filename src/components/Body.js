30d71f7e48186a28379dd0046d5f3b05a6e3c553
Jaskaran-Nanda <sahibsnanda@gmail.com>
2024-04-25 00:20:22 +0400

    Changes to be committed:

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,33 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 const Body = () => {
   // State Variable - Super Powerfull Variable - for that we use Hooks
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  // Use Effect Hook
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8541536&lng=80.94478269999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    // Optional Chaining
+    setListOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
   //
-  const [listOfRestaurants, setListOfRestaurant] = useState(resList);
-  //
+  /* if (listOfRestaurants.length === 0) {
+    return <h1>Loading.....</h1>;
+  } */
+  if (listOfRestaurants.length === 0) {
+    return <Shimmer />;
+  }
   return (
     <div className="body">
       <div className="filter">
