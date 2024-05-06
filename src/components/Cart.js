a75576bdfc5c242c25c4508aa2d91a96d4429e89
Jaskaran-Nanda <sahibsnanda@gmail.com>
2024-05-06 23:57:54 +0400

    implemented cart functionality, still have to fix some things

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,32 @@
+import { useDispatch } from "react-redux";
+import ItemList from "./ItemList";
+import { useSelector } from "react-redux";
+import { clearCart } from "../utils/cartSlice";
 const Cart = () => {
+  const cartItems = useSelector((store) => store.cart.items);
+  const dispatch = useDispatch();
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
   return (
     <div className="m-6 p-8">
-      <h1 className="text-xl font-bold">Cart</h1>
+      <div className="flex justify-between">
+        <h1 className="text-xl font-bold">Cart</h1>
+        <button
+          className="p-2 m-2 bg-black text-white rounded-xl"
+          onClick={handleClearCart}
+        >
+          Clear Cart
+        </button>
+      </div>
+      <div className="w-6/12 m-auto flex">
+        {cartItems.length === 0 && (
+          <h1 className="m-auto">
+            Your Cart is Empty, Please add items to the cart
+          </h1>
+        )}
+        <ItemList items={cartItems} />
+      </div>
     </div>
   );
 };
